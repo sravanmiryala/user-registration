5dc88350e66dc550811e580ff1eb150c5c3d9a75
sravanmiryala <smiryala1@gmail.com>
2025-10-22 23:39:10 +0100

    Update UsersApplicationTests.java

--- src/test/java/com/murali/users/UsersApplicationTests.java
+++ src/test/java/com/murali/users/UsersApplicationTests.java
@@ -1,10 +1,12 @@
 package com.cloudawsdevops.users;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class UsersApplicationTests {
 
     @Test
