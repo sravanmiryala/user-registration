4fc59ccc228908b42060830fc442fd4ccc9846a0
sravanmiryala <smiryala1@gmail.com>
2025-10-22 23:36:31 +0100

    Update UsersApplicationTests.java

--- src/test/java/com/murali/users/UsersApplicationTests.java
+++ src/test/java/com/murali/users/UsersApplicationTests.java
@@ -1,13 +1,14 @@
 package com.cloudawsdevops.users;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(exclude = {DataSourceAutoConfiguration.class})
 class UsersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
