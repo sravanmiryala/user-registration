7f39f678f744c6b38fe7d682db744130164a4667
sravanmiryala <smiryala1@gmail.com>
2025-10-22 23:46:54 +0100

    Update UsersApplicationTests.java

--- src/test/java/com/murali/users/UsersApplicationTests.java
+++ src/test/java/com/murali/users/UsersApplicationTests.java
@@ -1,14 +1,6 @@
-package com.cloudawsdevops.users;
-
-import com.cloudawsdevops.users.repositories.UserRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 @SpringBootTest
 class UsersApplicationTests {
 
-    // Mock the repository so Spring can autowire UserService without a real DB
     @MockBean
     private UserRepository userRepository;
 
