90f68b97a60ebbc459313be3aec1748c6836c245
sravanmiryala <smiryala1@gmail.com>
2025-10-22 23:48:02 +0100

    Update UsersApplicationTests.java

--- src/test/java/com/murali/users/UsersApplicationTests.java
+++ src/test/java/com/murali/users/UsersApplicationTests.java
@@ -1,3 +1,10 @@
+package com.cloudawsdevops.users;
+
+import com.cloudawsdevops.users.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 @SpringBootTest
 class UsersApplicationTests {
 
