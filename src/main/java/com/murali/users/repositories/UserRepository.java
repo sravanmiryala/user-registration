037fdf494a8e6a6010f4a6bbb0503f3056fb8905
sravanmiryala <smiryala1@gmail.com>
2025-10-22 23:16:11 +0100

    Update UserRepository.java

--- src/main/java/com/murali/users/repositories/UserRepository.java
+++ src/main/java/com/murali/users/repositories/UserRepository.java
@@ -1,9 +1,10 @@
-package com.murali.users.repositories;
+package com.cloudawsdevops.users.repositories;
 
-import com.murali.users.entities.User;
+import com.cloudawsdevops.users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
+
