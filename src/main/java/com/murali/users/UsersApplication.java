75b4994ac226737c52334f7904c12adc726ee8a7
sravanmiryala <smiryala1@gmail.com>
2025-10-22 23:11:50 +0100

    Update UsersApplication.java

--- src/main/java/com/murali/users/UsersApplication.java
+++ src/main/java/com/murali/users/UsersApplication.java
@@ -1,4 +1,4 @@
-package com.murali.users;
+package com.cloudawsdevops.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
